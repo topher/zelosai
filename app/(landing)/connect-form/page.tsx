7f1a960ac86204e659761f5213464fb620f7956c
Chris <chris@zelos.ai>
2024-07-15 12:22:49 -0400

    updated menu for an interest survery and separate get started button

---
+++ app/(landing)/connect-form/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ConnectForm = () => {
+    return (
+      <>
+      <iframe
+        src="https://form.typeform.com/to/MsoDhz0P"
+        style={{ width: '100%', height: '100vh', border: 'none' }}
+        title="typeform"
+      />
+      </>
+    );
+  };
+  
+  export default ConnectForm;
--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -24,9 +24,14 @@ export const LandingNavbar = () => {
       </Link>
       <div className="flex items-center gap-x-2">
         <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
-        <Button className="px-6 py-6 text-xl font-bold rounded-xl bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-white shadow-lg transform transition duration-300 hover:scale-110 hover:shadow-xl">
-          Get Started
-        </Button>
+          <Button className="px-6 py-6 text-xl font-bold rounded-xl border-2 border-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-white bg-transparent shadow-lg transform transition duration-300 hover:scale-110 hover:shadow-xl">
+            Log In
+          </Button>
+        </Link>
+        <Link href="/connect-form">
+          <Button className="px-6 py-6 text-xl font-bold rounded-xl bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-white shadow-lg transform transition duration-300 hover:scale-110 hover:shadow-xl">
+            Get Started
+          </Button>
         </Link>
       </div>
     </nav>
