eafc3eb5eba7241e1ccd1c035aada954fef69d27
Jake Johnson <jkjohnson1996@gmail.com>
2024-07-24 22:02:56 -0700

    removed extra navy background upon sidebar collapse

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -15,7 +15,8 @@ const DashboardLayout = ({
 
   return (
     <div className="min-h-screen relative bg-cover bg-center bg-fixed">
-      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-80 bg-gray-900">
+      <div>
+      {/* className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-80 bg-gray-900" */}
         <Sidebar isPro={isPro} apiLimitCount={apiLimitCount} />
       </div>
       <main className="md:pl-72 min-h-screen">
