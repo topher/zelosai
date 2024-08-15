ccbd130408d003a6b09f830c50cc5680cb7becc4
Jake Johnson <jkjohnson1996@gmail.com>
2024-08-14 20:38:18 -0700

    comment cleanup pre merge with main

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -10,15 +10,15 @@ const LandingPage = () => {
     <div className="h-full">
       <LandingNavbar />
       <LandingHero />
-      <div className="w-full h-[1px] bg-white/20 mt-16"></div> {/* Thin White Line */}
+      <div className="w-full h-[1px] bg-white/20 mt-16"></div>
       <div id="features" className="pt-16 -mt-16">
         <LandingFeatures />
       </div>
-      <div className="w-full h-[1px] bg-white/20 mt-16"></div> {/* Thin White Line */}
+      <div className="w-full h-[1px] bg-white/20 mt-16"></div>
       <div id="process" className="pt-16 -mt-16">
         <LandingProcess />
       </div>
-      <div className="w-full h-[1px] bg-white/20 mt-16"></div> {/* Thin White Line */}
+      <div className="w-full h-[1px] bg-white/20 mt-16"></div>
       <div id="pricing" className="pt-16 -mt-16">
         <PricingSection />
       </div>
