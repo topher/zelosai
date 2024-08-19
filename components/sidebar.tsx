b4ee2dbf13fadb41a9fc73baf7dd81d927cf5962
Jake Johnson <jkjohnson1996@gmail.com>
2024-08-19 14:05:00 -0700

    added dashboard homepage

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -18,10 +18,10 @@ export const routes = [
     href: '/dashboard',
     color: "text-white-500",
     children: [
-      { label: 'Assistants', href: '/digital-twin', color: "text-darkGray", description: "Explore your collection of AI models.", icon: "folder" },
-      { label: 'Data Vault', href: '/knowledge-bank', color: "text-darkGray", description: "Monitor the performance of your AI models.", icon: "chart" },
-      { label: 'Workflows', href: '/workflows', color: "text-darkGray", description: "Ensure the safe and responsible use of your likeness.", icon: "lock" },
-      { label: 'Strategy', href: '/strategy', color: "text-darkGray", description: "Build new AI models to expand your digital brand.", icon: "new-folder" },
+      { label: 'Assistants', href: '/digital-twin', color: "text-darkGray", description: "Explore your collection of AI models.", icon: "picture" },
+      { label: 'Data Vault', href: '/knowledge-bank', color: "text-darkGray", description: "Monitor the performance of your AI models.", icon: "locker" },
+      { label: 'Workflows', href: '/workflows', color: "text-darkGray", description: "Ensure the safe and responsible use of your likeness.", icon: "bulb" },
+      { label: 'Strategy', href: '/strategy', color: "text-darkGray", description: "Build new AI models to expand your digital brand.", icon: "travel" },
     ],
   },
   {
